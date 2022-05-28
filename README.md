01c9ab8e9f890ecd601999b19f0b0072d22b8143
Alexander <sajget@ya.ru>
2022-05-28 12:17:30 +0300

    Tests & readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # example-signature
-Example how to calculate signature of a very big file
+Example how to calculate signature of a very big file.
+
+The main idea is to use memory mapped files, because
+they suit that purpose perfectly - you don't need to
+manage file chunks manually, all that stuff will be
+done by the OS.
+
+## How to build
+1. `mkdir ../example-signature-build`
+2. `cd ../example-signature-build`
+3. `conan install ../example-signature -s compiler.libcxx=libstdc++11 --build=boost` -
+there are some Linux specific flags, but should be possible to replicate on Windows.
+4. `cmake ../example-signature`
+5. `cmake --build .`
+6. Now there will be `bin` folder where you can find binary & tests binary. Tests binary
+expect that working directory will be `tests/data` (yeah, a little bit inconvenient).
+
+## Usage
+`example-signature input_file output_file 1024`
+
+## Known limitations which can be improved
+1. Output file will be executable.
+2. Input file cannot be empty.
+3. Argparsing isn't done properly.
--- tests/signature_tests.cpp
+++ tests/signature_tests.cpp
@@ -1,8 +1,45 @@
 #include "signature.h"
 
-#include <gtest/gtest.h>
+#include <fstream>
 
-TEST(Example, Basic) {
-    // TODO: tests
-    ASSERT_EQ(43, 43);
+#include <gmock/gmock.h>
+
+using ::testing::HasSubstr;
+using ::testing::ThrowsMessage;
+
+TEST(Example, BadFiles) {
+    EXPECT_THAT(
+        [] {
+            signature::Signer("not_exist1", "output1", 1, 1);
+        },
+        ThrowsMessage<signature::SignerError>(HasSubstr("Unable to open input file"))
+    );
+    EXPECT_THAT(
+        [] {
+            signature::Signer("empty", "output1", 1, 1);
+        },
+        ThrowsMessage<signature::SignerError>(HasSubstr("Unable to open input file"))
+    );
+
+    std::remove("output1");
 }
+
+TEST(Example, SmallFile) {
+    signature::Signer signer("test", "output2", 1, 10);
+    signer.run();
+
+    std::ifstream file("output2");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    EXPECT_EQ(
+        buffer.str(),
+        std::string(
+            "\xb7\xdb\xed\xf6\x0d\x06\x83\xc1\x9b\x4d\xa6\xd3\x38\x9c\xce\x67"
+            "\xae\xd7\xeb\x75\x14\x0a\x85\x42",
+            6 * 4)
+    );
+
+    std::remove("output2");
+}
+
+// TODO: big file test
